403b3659c6c0b9e50a6429bbd56595607144b9aa
David Harestad <davidharestad@gmail.com>
2018-07-13 13:49:12 +0200

    helseapp firebase

---
+++ public/app.js
@@ -0,0 +1,16 @@
+var list = document.getElementById("listePlass");
+
+function changeText2() {
+    var brukerName = document.getElementById("brukerNavn").value;
+    var brukerId = document.getElementById("brukerId").value;
+
+    if(brukerName ==="" || brukerId ===""){
+        alert("Mangler input, vennligst skriv inn relevant info!"); 
+    }else{
+    var entry = document.createElement('li');
+    entry.appendChild(document.createTextNode(brukerName + " " + " med id: " + brukerId));
+    list.appendChild(entry); 
+    }
+}
+
+
